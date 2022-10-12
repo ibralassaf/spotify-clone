1e8ea957d7ed09e40aae8e213234df42778df173
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-13 00:14:40 +0300

    style: change image and button style

--- pages/login.js
+++ pages/login.js
@@ -8,14 +8,14 @@ export default function login({ providers }) {
       <Image
         className="mb-5 w-52"
         src="https://links.papareact.com/9xl"
-        height={200}
-        width={200}
+        height={150}
+        width={150}
         alt=""
       />
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
-            className="bg-[#18D860] text-white p-5 mt-5 rounded-full"
+            className="w-full mt-5 text-white bg-[#1ed760] hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
             Login with {provider.name}
