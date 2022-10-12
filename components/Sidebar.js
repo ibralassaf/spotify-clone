20491f8a92a0615720f3675510957f8c4dbff7f3
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-13 00:14:02 +0300

    feat: add signOut functionality

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,11 +6,20 @@ import {
   RssIcon,
   SearchIcon,
 } from "@heroicons/react/outline";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Sidebar() {
+  const { data: session, status } = useSession();
+
   return (
     <div className="p-5 text-sm text-gray-500 border-r border-gray-900">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Logout</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="w-5 h-5" />
           <p>Home</p>
