5fdd63187423098686493735c92f1308e47793b5
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-12 20:48:34 +0300

    feat: update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,37 @@
+import { getProviders, signIn } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
 
-export default function login() {
-  return <div>This is login page</div>;
+export default function login({ providers }) {
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-black">
+      <Image
+        className="mb-5 w-52"
+        src="https://links.papareact.com/9xl"
+        height={200}
+        width={200}
+        alt=""
+      />
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18D860] text-white p-5 mt-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
 }
