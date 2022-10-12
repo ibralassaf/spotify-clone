e57e63d54edf0ca32d22bc5930d4cef3ee6aff44
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-12 20:14:05 +0300

    feat: update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -63,6 +63,14 @@ export const authOptions = {
       console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
       return await refreshAccessToken(token);
     },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    },
   },
 };
 
