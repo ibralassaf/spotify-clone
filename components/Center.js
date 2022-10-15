733d296eaf3c9b13d055add523e607aeaf0e602d
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-15 16:58:49 +0300

    feat: create Center.js

---
+++ components/Center.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Center() {
+  return (
+    <div className="flex flex-grow">
+      <h1 className="text-white">I am center</h1>
+    </div>
+  );
+}
