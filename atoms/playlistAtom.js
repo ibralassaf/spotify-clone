a18159380bd80cf47ce8323e1d54bf8c49c13aed
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-16 00:55:31 +0300

    feat: create playlistAtom.js

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+  key: "playlistAtomState",
+  default: null,
+});
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "1oX8Opj7hr10BUbJqReuxA",
+});
