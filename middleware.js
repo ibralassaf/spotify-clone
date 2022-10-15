9c231528cdb155f3d9bc16eee98d8ba192e368ab
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-15 16:58:40 +0300

    feat: update middleware.js

--- middleware.js
+++ middleware.js
@@ -16,10 +16,18 @@ export async function middleware(req) {
   if (pathname.includes("/api/auth") || token) {
     return NextResponse.next();
   }
+
   // Redirect them to login if they don't have a token and are requesting a protected route
 
   if (!token && pathname !== "/login") {
     url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  // Redirect them to home if they have a token and are requesting a public route
+  // (Not Working)
+  if (token && pathname === "/login") {
+    url.pathname = "/";
     return NextResponse.redirect(url);
   }
 }
