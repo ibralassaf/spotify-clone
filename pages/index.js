69db542de5acfe2b8e21118f05923e813cefdf5d
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-15 16:59:02 +0300

    feat: update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
+import Center from "../components/Center";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="h-screen overflow-hidden bg-black">
+    <div className="h-screen overflow-hidden bg-black">
+      <main className="flex ">
         <Sidebar />
-        {/* Center */}
+        <Center />
       </main>
 
       <div>{/* Player */}</div>
