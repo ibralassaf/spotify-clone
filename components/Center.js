67b17b1f63edd8ff6e659e8ff27483c25ea2d546
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-16 00:55:36 +0300

    feat: update Center.js

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,11 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { shuffle } from "lodash";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
 
 const colors = [
   "from-indigo-500",
@@ -11,15 +15,27 @@ const colors = [
   "from-red-500",
   "from-pink-500",
   "from-purple-500",
+  "from-gray-500",
 ];
 
 export default function Center() {
   const { data: session } = useSession();
+  const spotifyApi = useSpotify();
   const [color, setColor] = useState(null);
+  const playlistId = useRecoilValue(playlistIdState); // read only state
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
 
   useEffect(() => {
-    setColor(shuffle(colors).pop()); // it shuffles the array and pop value of the array
-  }, []);
+    spotifyApi
+      .getPlaylist(playlistId)
+      .then((data) => {
+        setPlaylist(data.body);
+        setColor(shuffle(colors).pop()); // it shuffles the array and pop value of the array
+      })
+      .catch((err) => {
+        console.log("Something went wrong", err);
+      });
+  }, [spotifyApi, playlistId, setPlaylist]);
 
   return (
     <div className="flex-grow ">
@@ -39,8 +55,22 @@ export default function Center() {
       <section
         className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
       >
-        {/* <Image src={ } alt="" width={300} height={300} /> */}
-        <h1>Hello</h1>
+        {/* <img
+          src={playlist?.images?.[0]?.url}
+          alt=""
+          className="shadow-2xl h-44 w-44"
+        /> */}
+        <Image
+          src={playlist?.images?.[0]?.url}
+          alt=""
+          className="shadow-2xl h-44 w-44"
+          width={176}
+          height={176}
+        ></Image>
+        <div>
+          <p>PLAYLIST</p>
+          <h1 className="text-2xl md:text-3xl xl:text-5xl">{playlist?.name}</h1>
+        </div>
       </section>
     </div>
   );
