48030361ccc2b693c1d81b756847e8692e2cf1dc
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-17 00:50:42 +0300

    feat: update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+import { getSession } from "next-auth/react";
 import Center from "../components/Center";
+import Player from "../components/Player";
 import Sidebar from "../components/Sidebar";
-
 export default function Home() {
   return (
     <div className="h-screen overflow-hidden bg-black">
@@ -9,7 +10,19 @@ export default function Home() {
         <Center />
       </main>
 
-      <div>{/* Player */}</div>
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
