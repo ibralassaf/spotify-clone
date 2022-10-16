3ed4afbefd4daed11e0b663d988a8ce0d4713eaf
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-17 00:50:18 +0300

    feat: update Center.js

--- components/Center.js
+++ components/Center.js
@@ -1,11 +1,12 @@
-import { ChevronDownIcon } from "@heroicons/react/outline";
+import { ArrowRightIcon } from "@heroicons/react/outline";
 import { shuffle } from "lodash";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
 import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 
 const colors = [
   "from-indigo-500",
@@ -38,40 +39,45 @@ export default function Center() {
   }, [spotifyApi, playlistId, setPlaylist]);
 
   return (
-    <div className="flex-grow ">
+    <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center p-1 pr-2 space-x-3 bg-black rounded-full cursor-pointer opacity-90 hover:opacity-80">
-          {/* <Image width={40} height={40}></Image> */}
-          <img
-            className="w-10 h-10 rounded-full"
+        <div
+          onClick={signOut}
+          className="flex items-center p-1 pr-2 space-x-3 text-white bg-black rounded-full cursor-pointer opacity-90 hover:opacity-80"
+        >
+          <Image
+            className="rounded-full"
             src={session?.user.image}
             alt=""
-          ></img>
+            width={40}
+            height={40}
+          ></Image>
           <h2> {session?.user.name}</h2>
-          <ChevronDownIcon className="w-5 h-5" />
+          <ArrowRightIcon className="w-5 h-5" />
         </div>
       </header>
 
       <section
         className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
       >
-        {/* <img
-          src={playlist?.images?.[0]?.url}
-          alt=""
-          className="shadow-2xl h-44 w-44"
-        /> */}
         <Image
           src={playlist?.images?.[0]?.url}
           alt=""
-          className="shadow-2xl h-44 w-44"
+          className="shadow-4xl h-44 w-44"
           width={176}
           height={176}
         ></Image>
         <div>
-          <p>PLAYLIST</p>
-          <h1 className="text-2xl md:text-3xl xl:text-5xl">{playlist?.name}</h1>
+          <p className="text-sm">PLAYLIST</p>
+          <h1 className="text-2xl font-bold md:text-3xl xl:text-6xl">
+            {playlist?.name}
+          </h1>
         </div>
       </section>
+
+      <div>
+        <Songs />
+      </div>
     </div>
   );
 }
