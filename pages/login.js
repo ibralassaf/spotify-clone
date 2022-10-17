995ec4329861ef892b7a0926596fc7dd3e574385
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-17 20:35:17 +0300

    feat: add Head tag and title for the login page

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,14 @@
 import { getProviders, signIn } from "next-auth/react";
+import Head from "next/head";
 import Image from "next/image";
 import React from "react";
 
 export default function login({ providers }) {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen bg-black">
+      <Head>
+        <title>Spotify Clone - Login</title>
+      </Head>
       <Image
         className="mb-5 w-52"
         src="https://links.papareact.com/9xl"
