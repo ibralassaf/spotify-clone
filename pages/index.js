a4860b16d1f35b31627ec02fd4332928568a89e3
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-17 20:32:34 +0300

    feat: add title tag

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { getSession } from "next-auth/react";
+import Head from "next/head";
 import Center from "../components/Center";
 import Player from "../components/Player";
 import Sidebar from "../components/Sidebar";
 export default function Home() {
   return (
     <div className="h-screen overflow-hidden bg-black">
+      <Head>
+        <title>Spotify Clone</title>
+      </Head>
       <main className="flex ">
         <Sidebar />
         <Center />
